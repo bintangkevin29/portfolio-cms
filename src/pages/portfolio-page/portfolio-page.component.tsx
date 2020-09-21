519605258bca0c3b3ff2fb748335bb9dfdbf51ad
Kevin Samosir <bintangkevin29@gmail.com>
2020-09-22 00:13:38 +0700

    added portfolip page

--- src/pages/portfolio-page/portfolio-page.component.tsx
+++ src/pages/portfolio-page/portfolio-page.component.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { Table } from "react-bootstrap";
+import CustomCard from "../../components/card";
 
 const PortfolioPage: React.FC = () => {
-  return <div>Hello</div>;
+  return (
+    <CustomCard colXs={12}>
+      <Table bordered>
+        <thead>
+          <tr>
+            <th>Tes</th>
+            <th>Tes</th>
+            <th>Tes</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+          </tr>
+        </tbody>
+      </Table>
+    </CustomCard>
+  );
 };
 
 export default PortfolioPage;
